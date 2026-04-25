73e9462732726b31599e90dff8c572a06c972483
abhiman-repos <abhiigamerz420@gmail.com>
2026-04-25 22:33:28 +0530

    pushing the production ready frontend with implementing some minor things

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,15 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const host = request.headers.get("host") || "";
 
+  // 🔥 If admin subdomain
   if (host.startsWith("admin.")) {
-    return NextResponse.rewrite(new URL("/admin", request.url));
+    const url = request.nextUrl.clone();
+
+    // prevent infinite loop
+    if (!url.pathname.startsWith("/admin")) {
+      url.pathname = "/admin";
+      return NextResponse.rewrite(url);
+    }
   }
 
   return NextResponse.next();
